1b4bdf955cf79f2b5fb18b72bfa7cc3c4d11ab71
jpchanamolu <prakash.axapta@gmail.com>
2015-10-23 21:49:39 -0400

    2nd commit

--- src/org/jfree/chart/axis/LogAxis.java
+++ src/org/jfree/chart/axis/LogAxis.java
@@ -90,7 +90,7 @@ import org.jfree.ui.TextAnchor;
  *
  * @since 1.0.7
  */
-public class LogAxis extends ValueAxis {
+public class LogAxis extends IntermediateValueAxis {
 
     /** The logarithm base. */
     private double base = 10.0;
@@ -764,38 +764,7 @@ public class LogAxis extends ValueAxis {
     protected double estimateMaximumTickLabelWidth(Graphics2D g2, 
             TickUnit unit) {
 
-        RectangleInsets tickLabelInsets = getTickLabelInsets();
-        double result = tickLabelInsets.getLeft() + tickLabelInsets.getRight();
-
-        if (isVerticalTickLabels()) {
-            // all tick labels have the same width (equal to the height of the
-            // font)...
-            FontRenderContext frc = g2.getFontRenderContext();
-            LineMetrics lm = getTickLabelFont().getLineMetrics("0", frc);
-            result += lm.getHeight();
-        }
-        else {
-            // look at lower and upper bounds...
-            FontMetrics fm = g2.getFontMetrics(getTickLabelFont());
-            Range range = getRange();
-            double lower = range.getLowerBound();
-            double upper = range.getUpperBound();
-            String lowerStr, upperStr;
-            NumberFormat formatter = getNumberFormatOverride();
-            if (formatter != null) {
-                lowerStr = formatter.format(lower);
-                upperStr = formatter.format(upper);
-            }
-            else {
-                lowerStr = unit.valueToString(lower);
-                upperStr = unit.valueToString(upper);
-            }
-            double w1 = fm.stringWidth(lowerStr);
-            double w2 = fm.stringWidth(upperStr);
-            result += Math.max(w1, w2);
-        }
-
-        return result;
+        return estimateMaximumTickLabelWidthExtracted(g2, unit);
 
     }
 
--- src/org/jfree/chart/axis/NumberAxis.java
+++ src/org/jfree/chart/axis/NumberAxis.java
@@ -139,7 +139,7 @@ import org.jfree.util.ObjectUtilities;
  * selecting a tick unit that is appropriate for the current axis range.  This
  * mechanism is an adaptation of code suggested by Laurence Vanhelsuwe.
  */
-public class NumberAxis extends ValueAxis implements Cloneable, Serializable {
+public class NumberAxis extends IntermediateValueAxis implements Cloneable, Serializable {
 
     /** For serialization. */
     private static final long serialVersionUID = 2805933088476185789L;
@@ -986,38 +986,7 @@ public class NumberAxis extends ValueAxis implements Cloneable, Serializable {
     protected double estimateMaximumTickLabelWidth(Graphics2D g2,
                                                    TickUnit unit) {
 
-        RectangleInsets tickLabelInsets = getTickLabelInsets();
-        double result = tickLabelInsets.getLeft() + tickLabelInsets.getRight();
-
-        if (isVerticalTickLabels()) {
-            // all tick labels have the same width (equal to the height of the
-            // font)...
-            FontRenderContext frc = g2.getFontRenderContext();
-            LineMetrics lm = getTickLabelFont().getLineMetrics("0", frc);
-            result += lm.getHeight();
-        }
-        else {
-            // look at lower and upper bounds...
-            FontMetrics fm = g2.getFontMetrics(getTickLabelFont());
-            Range range = getRange();
-            double lower = range.getLowerBound();
-            double upper = range.getUpperBound();
-            String lowerStr, upperStr;
-            NumberFormat formatter = getNumberFormatOverride();
-            if (formatter != null) {
-                lowerStr = formatter.format(lower);
-                upperStr = formatter.format(upper);
-            }
-            else {
-                lowerStr = unit.valueToString(lower);
-                upperStr = unit.valueToString(upper);
-            }
-            double w1 = fm.stringWidth(lowerStr);
-            double w2 = fm.stringWidth(upperStr);
-            result += Math.max(w1, w2);
-        }
-
-        return result;
+        return estimateMaximumTickLabelWidthExtracted(g2, unit);
 
     }
 
