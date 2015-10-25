1b435ae4213ce6c3ca0d65b4b033ce4df36562ca
jpchanamolu <prakash.axapta@gmail.com>
2015-10-24 23:48:58 -0400

    13th_commit

--- src/org/jfree/chart/plot/PiePlot.java
+++ src/org/jfree/chart/plot/PiePlot.java
@@ -3096,7 +3096,14 @@ public class PiePlot extends Plot implements Cloneable, Serializable {
         double targetX = anchorX - record.getGap();
         double targetY = record.getAllocatedY();
 
-        if (this.labelLinksVisible) {
+        TextBox tb = drawLeftRightLabel(g2, state, record, anchorX, targetX, targetY);
+        tb.draw(g2, (float) targetX, (float) targetY, RectangleAnchor.RIGHT);
+
+    }
+
+	private TextBox drawLeftRightLabel(Graphics2D g2, PiePlotState state, PieLabelRecord record, double anchorX,
+			double targetX, double targetY) {
+		if (this.labelLinksVisible) {
             double theta = record.getAngle();
             double linkX = state.getPieCenterX() + Math.cos(theta)
                     * state.getPieWRadius() * record.getLinkPercent();
@@ -3129,9 +3136,8 @@ public class PiePlot extends Plot implements Cloneable, Serializable {
             }
         }
         TextBox tb = record.getLabel();
-        tb.draw(g2, (float) targetX, (float) targetY, RectangleAnchor.RIGHT);
-
-    }
+		return tb;
+	}
 
     /**
      * Draws a section label on the right side of the pie chart.
@@ -3147,40 +3153,7 @@ public class PiePlot extends Plot implements Cloneable, Serializable {
         double targetX = anchorX + record.getGap();
         double targetY = record.getAllocatedY();
 
-        if (this.labelLinksVisible) {
-            double theta = record.getAngle();
-            double linkX = state.getPieCenterX() + Math.cos(theta)
-                    * state.getPieWRadius() * record.getLinkPercent();
-            double linkY = state.getPieCenterY() - Math.sin(theta)
-                    * state.getPieHRadius() * record.getLinkPercent();
-            double elbowX = state.getPieCenterX() + Math.cos(theta)
-                    * state.getLinkArea().getWidth() / 2.0;
-            double elbowY = state.getPieCenterY() - Math.sin(theta)
-                    * state.getLinkArea().getHeight() / 2.0;
-            double anchorY = elbowY;
-            g2.setPaint(this.labelLinkPaint);
-            g2.setStroke(this.labelLinkStroke);
-            PieLabelLinkStyle style = getLabelLinkStyle();
-            if (style.equals(PieLabelLinkStyle.STANDARD)) {
-                g2.draw(new Line2D.Double(linkX, linkY, elbowX, elbowY));
-                g2.draw(new Line2D.Double(anchorX, anchorY, elbowX, elbowY));
-                g2.draw(new Line2D.Double(anchorX, anchorY, targetX, targetY));
-            }
-            else if (style.equals(PieLabelLinkStyle.QUAD_CURVE)) {
-                QuadCurve2D q = new QuadCurve2D.Float();
-                q.setCurve(targetX, targetY, anchorX, anchorY, elbowX, elbowY);
-                g2.draw(q);
-                g2.draw(new Line2D.Double(elbowX, elbowY, linkX, linkY));
-            }
-            else if (style.equals(PieLabelLinkStyle.CUBIC_CURVE)) {
-                CubicCurve2D c = new CubicCurve2D .Float();
-                c.setCurve(targetX, targetY, anchorX, anchorY, elbowX, elbowY,
-                        linkX, linkY);
-                g2.draw(c);
-            }
-        }
-
-        TextBox tb = record.getLabel();
+        TextBox tb = drawLeftRightLabel(g2, state, record, anchorX, targetX, targetY);
         tb.draw(g2, (float) targetX, (float) targetY, RectangleAnchor.LEFT);
 
     }
