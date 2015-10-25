2c3285afb26edd777e70f1c76269da08fdaca6c5
jpchanamolu <prakash.axapta@gmail.com>
2015-10-25 14:50:03 -0400

    20th_commit

--- src/org/jfree/chart/renderer/category/Utility.java
+++ src/org/jfree/chart/renderer/category/Utility.java
@@ -3,6 +3,7 @@ package org.jfree.chart.renderer.category;
 
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RectangularShape;
+import java.io.Serializable;
 import org.jfree.ui.RectangleEdge;
 
 public class Utility {
@@ -42,5 +43,18 @@ public class Utility {
 			y1 += yOffset;
 		}
 		return new Rectangle2D.Double(x0, y0, (x1 - x0), (y1 - y0));
+	}
+
+	public static Rectangle2D[] splitVerticalBarExtracted(RectangularShape bar, double a, double b, double c) {
+		Rectangle2D[] result = new Rectangle2D[4];
+		double x0 = bar.getMinX();
+		double x1 = Math.rint(x0 + (bar.getWidth() * a));
+		double x2 = Math.rint(x0 + (bar.getWidth() * b));
+		double x3 = Math.rint(x0 + (bar.getWidth() * c));
+		result[0] = new Rectangle2D.Double(bar.getMinX(), bar.getMinY(), x1 - x0, bar.getHeight());
+		result[1] = new Rectangle2D.Double(x1, bar.getMinY(), x2 - x1, bar.getHeight());
+		result[2] = new Rectangle2D.Double(x2, bar.getMinY(), x3 - x2, bar.getHeight());
+		result[3] = new Rectangle2D.Double(x3, bar.getMinY(), bar.getMaxX() - x3, bar.getHeight());
+		return result;
 	}
 }
