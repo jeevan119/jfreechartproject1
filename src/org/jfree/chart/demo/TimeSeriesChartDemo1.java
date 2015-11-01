787d718b3a21edfc8ea7ef9e708783d74ebaec53
saketh7 <saketh.mudrakola7@gmail.com>
2015-11-01 00:08:59 -0400

    commit6

--- src/org/jfree/chart/demo/TimeSeriesChartDemo1.java
+++ src/org/jfree/chart/demo/TimeSeriesChartDemo1.java
@@ -142,27 +142,8 @@ public class TimeSeriesChartDemo1 extends ApplicationFrame {
      */
     private static XYDataset createDataset() {
 
-        TimeSeries s1 = new TimeSeries("L&G European Index Trust");
-        s1.add(new Month(2, 2001), 181.8);
-        s1.add(new Month(3, 2001), 167.3);
-        s1.add(new Month(4, 2001), 153.8);
-        s1.add(new Month(5, 2001), 167.6);
-        s1.add(new Month(6, 2001), 158.8);
-        s1.add(new Month(7, 2001), 148.3);
-        s1.add(new Month(8, 2001), 153.9);
-        s1.add(new Month(9, 2001), 142.7);
-        s1.add(new Month(10, 2001), 123.2);
-        s1.add(new Month(11, 2001), 131.8);
-        s1.add(new Month(12, 2001), 139.6);
-        s1.add(new Month(1, 2002), 142.9);
-        s1.add(new Month(2, 2002), 138.7);
-        s1.add(new Month(3, 2002), 137.3);
-        s1.add(new Month(4, 2002), 143.9);
-        s1.add(new Month(5, 2002), 139.8);
-        s1.add(new Month(6, 2002), 137.0);
-        s1.add(new Month(7, 2002), 132.8);
-
-        TimeSeries s2 = new TimeSeries("L&G UK Index Trust");
+        TimeSeries s1 = s1();
+		TimeSeries s2 = new TimeSeries("L&G UK Index Trust");
         s2.add(new Month(2, 2001), 129.6);
         s2.add(new Month(3, 2001), 123.2);
         s2.add(new Month(4, 2001), 117.2);
@@ -197,6 +178,29 @@ public class TimeSeriesChartDemo1 extends ApplicationFrame {
         return dataset;
 
     }
+
+	private static TimeSeries s1() {
+		TimeSeries s1 = new TimeSeries("L&G European Index Trust");
+		s1.add(new Month(2, 2001), 181.8);
+		s1.add(new Month(3, 2001), 167.3);
+		s1.add(new Month(4, 2001), 153.8);
+		s1.add(new Month(5, 2001), 167.6);
+		s1.add(new Month(6, 2001), 158.8);
+		s1.add(new Month(7, 2001), 148.3);
+		s1.add(new Month(8, 2001), 153.9);
+		s1.add(new Month(9, 2001), 142.7);
+		s1.add(new Month(10, 2001), 123.2);
+		s1.add(new Month(11, 2001), 131.8);
+		s1.add(new Month(12, 2001), 139.6);
+		s1.add(new Month(1, 2002), 142.9);
+		s1.add(new Month(2, 2002), 138.7);
+		s1.add(new Month(3, 2002), 137.3);
+		s1.add(new Month(4, 2002), 143.9);
+		s1.add(new Month(5, 2002), 139.8);
+		s1.add(new Month(6, 2002), 137.0);
+		s1.add(new Month(7, 2002), 132.8);
+		return s1;
+	}
 
     /**
      * Creates a panel for the demo (used by SuperDemo.java).
